81a0a949d765da5d12fc61e8d8f098184460a31d
hectorflores329 <hectorflores329@gmail.com>
2021-04-28 00:15:44 -0600

    Commit 35

--- carpetas.py
+++ carpetas.py
@@ -86,6 +86,10 @@ def estructura():
                 else:
                     try:
                         os.mkdir(ruta2)
+
+                        destino = ruta2 + "/palta.png"
+                        shutil.copyfile(fuente, destino)
+
                     except:
                         pass
                 query = "SELECT * FROM PRODUCTO WHERE SECTOR_id = " + str(df2["id"][j])
@@ -108,6 +112,10 @@ def estructura():
                     
                             try:
                                 os.mkdir(ruta3)
+
+                                destino = ruta3 + "/palta.png"
+                                shutil.copyfile(fuente, destino)
+
                             except:
                                 pass
                         query = "SELECT * FROM CATEGORIA WHERE PRODUCTO_id = " + str(df3["id"][k])
@@ -130,6 +138,7 @@ def estructura():
                                         os.mkdir(ruta4)
                                     except:
                                         pass
+
                                 destino = ruta4 + "/palta.png"
                                 shutil.copyfile(fuente, destino)
                                 #palta.to_csv(ruta4 + "/palta.csv", index=False)
