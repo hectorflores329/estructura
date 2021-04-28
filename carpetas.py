4e99b9585747a9dc87280ba279b7276e751ca3c8
hectorflores329 <hectorflores329@gmail.com>
2021-04-27 18:57:40 -0600

    Commit 1

---
+++ carpetas.py
@@ -0,0 +1,8 @@
+def update():
+    print("Este es un texto de prueba")
+    
+
+if __name__ == '__main__':
+    print('Empezando proceso de descarga.')
+    update()
+    print('El roceso de descarga ha finalizado.')
